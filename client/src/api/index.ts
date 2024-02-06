a0bc106808739d711bc3670e6a5b88e66ab02577
Jason C Smith <smithones@gmail.com>
2024-02-06 15:04:26 -0700

    base api

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const baseURL = process.env.REACT_APP_BASE_URL
+const baseURL = process.env.REACT_APP_API_ENDPOINT
 
-const client = axios.create({ baseURL: 'https://api-gaxpa6b7ta-uc.a.run.app' })
+const client = axios.create({ baseURL })
 
 export default client
