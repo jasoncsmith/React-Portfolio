19cda8aa05775c2e884eff080c2695654379b4f6
Jason C Smith <smithones@gmail.com>
2024-03-01 18:25:42 -0700

    fix src

--- client/src/components/Logo/index.tsx
+++ client/src/components/Logo/index.tsx
@@ -6,7 +6,7 @@ function Logo() {
   return (
     <div className={styles.logo}>
       <NavLink to="/about">
-        <img src="/images/favicon.png" alt="Logo" width={32} height={32} />
+        <img src="images/favicon.png" alt="Logo" width={32} height={32} />
       </NavLink>
     </div>
   )
