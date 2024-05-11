3f385e53299aae0b1e7a27713ba68e400136fdf5
Jason C Smith <smithones@gmail.com>
2024-05-11 12:11:56 -0600

    fix pipeline

--- functions/src/index.js
+++ functions/src/index.js
@@ -5,10 +5,14 @@ import cors from 'cors'
 
 import projectRouter from './routes/projectRouter.js'
 import userRouter from './routes/userRouter.js'
+// const csurf = require('csurf')
+// const helmet = require("helmet")
 
 const app = express()
 const version = 'v1'
 
+// app.use(csurf())
+// app.use(helmet())
 app.use(express.json({ limit: '3mb' }))
 app.use(express.urlencoded({ limit: '3mb', extended: true }))
 app.use(cors())
