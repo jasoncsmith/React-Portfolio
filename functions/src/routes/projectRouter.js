c055eadc53409338bad612455c3fa3b9751c0924
Jason C Smith <smithones@gmail.com>
2024-09-01 14:05:53 -0600

    tailwind config changes, prettier rule, rate limiter

--- functions/src/routes/projectRouter.js
+++ functions/src/routes/projectRouter.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import getProjects from '../controllers/projectController.js'
+import rateLimiter from '../middleware/rateLimiter.js'
 
 const router = express.Router()
 
-router.route('/').get(getProjects)
+router.route('/').get(rateLimiter(5), getProjects)
 
 export default router
