3061910607f9d914f2c9c13e44749c12c6dd389a
jasoncsmith <jasonsmithnet@outlook.com>
2026-09-19 20:37:30 -0600

    msg change

--- client/src/components/ContactForm/components/SuccessMessage/index.tsx
+++ client/src/components/ContactForm/components/SuccessMessage/index.tsx
@@ -5,18 +5,10 @@ const SuccessMessage = ({ name = 'Visitor' }: { name: string }) => {
         Hello <strong style={{ color: '#eabb00', fontWeight: 'bold' }}>{name}</strong>,
       </p>
       <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm leading-5">
-        This form is wired up to a 🔥 <code>Firebase Firestore</code> NoSQL database on the backend and uses
-        the <strong style={{ color: '#eabb00', fontWeight: 'bold' }}>formik</strong> validation library on the
-        client. Source code is available at my{' '}
-        <strong style={{ color: '#eabb00', fontWeight: 'bold' }}>GitHub</strong> repo.{' '}
-        <a
-          className="text-blue-500"
-          href="https://github.com/jasoncsmith/React-Portfolio"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Check it out
-        </a>
+        This form is wired up to a 🔥 <code>Firebase Firestore</code>.
+      </p>
+      <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm leading-5">
+        Thank you for taking the time to reach out. I will be in touch within 2 business days.
       </p>
     </>
   )
